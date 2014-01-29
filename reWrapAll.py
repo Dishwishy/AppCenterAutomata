d6d0834850bfa0d011470c1e7314a4f96cc5bf30
Kyle Champlin <k.champlin@gmail.com>
2014-01-29 10:05:50 -0800

    updated reWrapAll to use requets now, instead of mechanize

--- reWrapAll.py
+++ reWrapAll.py
@@ -4,48 +4,39 @@
 #are already wrapped, find their wrap URL and request it
 #via mechanize, thus rewrapping everything
 
+from BeautifulSoup import BeautifulSoup
+import requests
+import json
+import time
 
-import mechanize, cookielib, json, time, argparse, getpass
+credentialFile = open('./creds.json')
+creds = json.load(credentialFile)
 
-parser = argparse.ArgumentParser(description='Tenant Info')
-parser.add_argument('--tenant', help='please enter your tenant name', required=True)
-parser.add_argument('--admin', help='enter the name of an admin account', required=True)
-args = parser.parse_args()
+tenantURL = creds['baseURL']
+username = creds['name']
+password = creds['pass']
+loginURL = '%s/admin/login' %tenantURL
+updateURL = '%s/admin/message/list' %tenantURL
 
-print "please enter the password for the admin account" 
-password = getpass.getpass()
+#create a requests session object
+s = requests.Session()
+#get our session cookies, headers, etc etc
+resp = s.get(loginURL)
+soup = BeautifulSoup(resp.content)
+form = soup.form
+csrfInfo = soup.find(type="hidden")
+csrfToken = csrfInfo['value']
 
-tenantURL = "https://" + args.tenant + ".appcenterhq.com"
+authPayload = {'csrfmiddlewaretoken':csrfToken,'username':username,'password':password,'login':'Login'}
+headerUpdate = {'Content-Type':'x-www-form-urlencoded'}
 
-br = mechanize.Browser()
+resp = s.post(loginURL, data=authPayload, headers=headerUpdate)
 
-#br.set_debug_redirects(True)
-#br.set_debug_responses(True)
-#br.set_debug_http(True)
 
-cj = cookielib.LWPCookieJar()
-br.set_cookiejar(cj)
 
-br.set_handle_equiv(1)
-br.set_handle_gzip(1)
-br.set_handle_redirect(1)
-br.set_handle_referer(1)
-br.set_handle_robots(0)
-
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-
-br.addheaders = [('User-agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36')]
-
-tenantAdminURL = '%s/admin/apps/list' %tenantURL
-print tenantAdminURL
-r = br.open(tenantAdminURL)
-br.select_form(nr=0)
-br.form['username'] = args.admin
-br.form['password'] = password
-br.submit()
 appWebService = '%s/appstore/webapi1/apps' %tenantURL
-appResponse = br.open(appWebService)
-appData = appResponse.read()
+appResponse = s.get(appWebService)
+appData = appResponse.content
 appDataJSON = json.loads(appData)
 
 print appDataJSON
@@ -56,7 +47,7 @@ for x in appDataJSON['apps']:
     print "now wrapping: " + x['title']
     rewrapURL = tenantURL + rewrapApp
     print rewrapURL
-    wrapResponse = br.open(rewrapURL)
+    wrapResponse = s.get(rewrapURL)
     print "Pausing for 10 seconds"
     time.sleep(10)
   #print x['policy']
